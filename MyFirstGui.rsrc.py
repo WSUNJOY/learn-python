9e126f533ff49f841929bfabae9816e8f0f644fc
SunJoy <1210972873@qq.com>
2016-06-21 17:35:33 +0800

    GUI界面

---
+++ MyFirstGui.rsrc.py
@@ -0,0 +1,37 @@
+{'application':{'type':'Application',
+          'name':'Template',
+    'backgrounds': [
+    {'type':'Background',
+          'name':'bgTemplate',
+          'title':'Standard Template with File->Exit menu',
+          'size':(400, 300),
+          'style':['resizeable'],
+
+        'menubar': {'type':'MenuBar',
+         'menus': [
+             {'type':'Menu',
+             'name':'menuFile',
+             'label':'&File',
+             'items': [
+                  {'type':'MenuItem',
+                   'name':'menuFileExit',
+                   'label':'E&xit',
+                   'command':'exit',
+                  },
+              ]
+             },
+         ]
+     },
+         'components': [
+
+{'type':'Button', 
+    'name':'helloButton', 
+    'position':(167, 86), 
+    'size':(87, 35), 
+    'label':u'Hello!', 
+    },
+
+] # end components
+} # end background
+] # end backgrounds
+} }
